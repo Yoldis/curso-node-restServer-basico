9d70baf8a261147f63a277dd1779cf0dbc68c87f
Yoldis Rodriguez <yoldisrodriguezherrera@gmail.com>
2023-08-31 21:04:54 -0400

    Agregando comentarios

--- models/server.js
+++ models/server.js
@@ -16,16 +16,18 @@ class Server {
     }
 
     middlewares(){
-        // Directorio publico
+        // Usamos la configuracion de cors, para que el frontend tenga persmisos a hacer solicitudes al backend
         this.app.use(cors());
         // Lectura y parseo del body - Es obligatorio,
         // es para aceptar peticiones en formato JSON a mi backend
         this.app.use(express.json());
+        // Para mostrar contenido estatico desde el backend, lo agregamos en la carpeta public
         this.app.use(express.static('public'));
     }
 
     routes(){
-       this.app.use(this.urlUsuarios, require('../routes/usuariosRouter'))
+        // Aqui agregamos todas nuestra rutas
+        this.app.use(this.urlUsuarios, require('../routes/usuariosRouter'));
     }
 
     listen(){
